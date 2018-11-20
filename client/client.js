912646b89af49c64d304bc01f6b876b083f629c7
caropi <pisanocaroline@gmail.com>
2018-11-20 14:01:37 -0500

    Modified client.js to sent push notification

--- client/client.js
+++ client/client.js
@@ -15,4 +15,36 @@ const publicVapidKey =
         });
         console.log('Service worker registered...');
         // register push
+        console.log('Registering push...');
+        const subscription = await register.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+        });
+        console.log('Push Registered...');
+
+        // we want to now send push notification
+        console.log('Sending Push...');
+        await fetch('/subscribe', {
+            method: 'POST',
+            body: JSON.stringify(subscription),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        console.log('Push sent...');
+    }
+
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; i++) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
     }
